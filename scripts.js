45e3308d70d878733686b2a5c075972a418169d0
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-17 17:11:10 +0000

    Added pages

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 window.onscroll = function scrolled() {
-    document.getElementById('background').style.opacity = (1-window.scrollY/document.documentElement.scrollHeight*2.5);
+    document.getElementById('background').style.opacity = (0.8-window.scrollY/document.documentElement.scrollHeight*3);
     if (window.scrollY > document.getElementById('background').height-20) {
         document.getElementById('header').classList.add('header');
     }
