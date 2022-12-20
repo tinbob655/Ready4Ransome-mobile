393230c924e7cd1b867b24ba53909c3787196ba2
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-20 14:51:54 +0000

    Mobile test #3

--- scripts-mobile.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function scrolled() {
-    document.getElementById('fade-with-scroll').style.opacity = (0.8-window.scrollY/document.documentElement.scrollHeight*5);
-    if (window.scrollY > document.getElementById('fade-with-scroll').height-20) {
-        document.getElementById('header').classList.add('header');
-    }
-    else if (window.scrollY <= document.getElementById('fade-with-scroll').height-20) {
-        document.getElementById('header').classList.remove('header');
-    };
-};
