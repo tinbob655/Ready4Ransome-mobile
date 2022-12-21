57f7cc354f2bce163ba61b1057a91c946d393a78
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-21 15:48:16 +0000

    Added menu button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 window.onscroll = function scrolled() {
     document.getElementById('background').style.opacity = (0.8-window.scrollY/document.documentElement.scrollHeight*3);
     if (window.scrollY > document.getElementById('background').height-20) {
-        document.getElementById('header').classList.add('header');
+        document.getElementById('header').classList.add('init');
+        document.getElementById('button-header').classList.add('menu');
     }
     else if (window.scrollY <= document.getElementById('background').height-20) {
-        document.getElementById('header').classList.remove('header');
+        document.getElementById('header').classList.remove('init');
+        document.getElementById('button-header').classList.remove('menu');
     };
+};
+
+function header_toggled() {
+    document.getElementById('header').classList.toggle('open');
 };
