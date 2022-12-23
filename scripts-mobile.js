40b9a28f184af5a892f3b997ca94d8d8cc4fb250
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-23 14:11:44 +0000

    Bugfixes

--- scripts-mobile.js
+++ scripts-mobile.js
@@ -2,10 +2,11 @@ function header_toggled () {
     document.getElementById('header').classList.toggle('opened');
 };
 
-window.onclick = function clicked() {
-    document.getElementById('audio').play();
-};
+//window.onclick = function clicked() {
+//    document.getElementById('audio').play();
+//};
 
 function introbox_cleared() {
     document.getElementById('introbox').classList.add('cleared');
+    document.getElementById('audio').play();
 };
