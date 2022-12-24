6b767e175fb94d8c8ce3d0e9f4062de43637066d
tinbob655 tinbob655 <wrutland07@gmail.com>
2022-12-24 17:19:57 +0000

    Added merch gallery to mobile

--- scripts-mobile.js
+++ scripts-mobile.js
@@ -18,7 +18,54 @@ function introbox_cleared() {
     document.getElementById('audio').play();
 };
 
-function gallery_refresh() {
+function is_in_bounds(lower_bound, upper_bound, input) {
+    if (input > upper_bound) {
+        var difference = input - upper_bound -1;
+        input = lower_bound + difference;
+    }
+    else if (input < lower_bound) {
+        var difference = lower_bound - input -1;
+        input = upper_bound - difference;
+    };
+    return(input);
 };
 
-//window.onload = init();
+function gallery_refresh() {
+    if (!document.getElementById('image1').src.includes('black%20hoodie')) {
+        document.getElementById('gallery').style.opacity = 0.0;
+        setTimeout(() => {
+            document.getElementById('image1').src = gallery_images[0];
+            document.getElementById('image2').src = gallery_images[1];
+            document.getElementById('image3').src = gallery_images[2];
+            gallery_num = 1;
+            document.getElementById('gallery').style.opacity = 1.0;
+        }, 700);
+    };
+};
+
+function gallery_move() {
+    document.getElementById('gallery').style.opacity = 0.0;
+    setTimeout(() => {
+        document.getElementById('image1').src = gallery_images[is_in_bounds(0, 8, gallery_num%9)];
+        document.getElementById('image2').src = gallery_images[is_in_bounds(0, 8, gallery_num%9 +1)];
+        document.getElementById('image3').src = gallery_images[is_in_bounds(0, 8, gallery_num%9 +2)];
+        gallery_num++;
+        document.getElementById('gallery').style.opacity = 1.0;
+    }, 700);
+};
+
+const gallery_images = [
+    'images/Merch/black hoodie.webp',
+    'images/Merch/black long sleeve t-shirt.webp',
+    'images/Merch/Black t-shirt.webp',
+    'images/Merch/blue t-shirt.webp',
+    'images/Merch/Todd is bad at golf t-shirt.webp',
+    'images/Merch/tote bag.webp',
+    'images/Merch/White Hoodie.webp',
+    'images/Merch/white long sleeve t-shirt.webp',
+    'images/Merch/white t-shirt.webp',
+];
+
+var gallery_num = 1;
+
+window.onload = init();
